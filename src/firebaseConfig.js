274a33856e3434fe792396fcb3687ec2bd16c574
Navyy0 <118597694+Navyy0@users.noreply.github.com>
2025-12-12 11:59:30 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@
 // Get these from Firebase console -> Project settings -> SDK setup and config
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDfWPSRUN0WHyWiZVz0BmX2dbzUjksT0zY",
-  authDomain: "ai-travel-4609c.firebaseapp.com",
-  projectId: "ai-travel-4609c",
-  storageBucket: "ai-travel-4609c.firebasestorage.app",
-  messagingSenderId: "999267496074",
-  appId: "1:999267496074:web:9c71b162e0135da037bd73",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
   
 }
 
